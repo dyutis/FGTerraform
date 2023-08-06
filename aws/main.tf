b3077b2a930b36712097ca5eefe9a2110ff0f16e
dyutis <51526942+dyutis@users.noreply.github.com>
2023-08-06 22:21:09 +0530

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -6,15 +6,14 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
+  region = "us-west-1"
 }
 
 resource "aws_s3_bucket" "fd_bucket" {
-  bucket = "fd-009-bucket"  # Replace with your desired bucket name
+  bucket = var.name  # Replace with your desired bucket name
   acl    = "private"
   }
 
-variable "aws_region" {
-  description = "The AWS region where the S3 bucket will be created."
-  default     = "us-east-1"  # You can set a default value, but it can be overridden during runtime.
+variable "name" {
+  description = "The bucket name"
 }
