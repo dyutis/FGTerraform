6f417d1e6392c0c192213897d42c2dd583357c37
dyutis <51526942+dyutis@users.noreply.github.com>
2023-09-05 22:53:01 +0530

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket" "fd_bucket" {
 
 output "s3_bucket_id" {
   description = "The name of the bucket."
-  value       = values(aws_s3_bucket.s3_bucket_id)
+  value       = aws_s3_bucket.s3_bucket_id
 }
 
 
